1fb922b9091a119e85ad50d5923dca8a57a8ec89
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-11-01 16:36:48 -0500

    class work

---
+++ lambda1.py
@@ -0,0 +1,62 @@
+import os
+
+clear = lambda: os.system("cls")
+clear()
+
+#def remainder(num):
+#   return num % 2
+
+remainder = lambda num: num % 2
+
+print(type(remainder))
+print(remainder(5)) #5 is the argument num
+
+product = lambda x,y: x*y
+print(product(2,3))
+
+
+def myfunction(num):
+    return lambda x: x* num
+
+result10 = myfunction(10)
+#result10 is a function
+result10 = lambda x: x *10
+#THE TWO RESULT10s ARE THE SAME
+
+result100 = myfunction(100)
+print(result10(9))
+print(result100(9))
+
+def myfunc(n):
+    return lambda a:a * n
+
+mydoubler = myfunc(2)   #this is n
+mytripler = myfunc(3)   #this is n
+
+print(mydoubler(11)) #this is a
+print(mytripler(11)) # this is a
+
+numbers = [2,4,6,8,10,3,18,14,21]
+
+filtered_list = list(filter(lambda num: (num > 7), numbers))
+print(filtered_list)
+
+mapped_list = list(map(lambda num: num % 2, numbers))
+
+print(mapped_list)
+
+x = lambda a: a + 10
+print(x(5))
+
+x = lambda a,b,c: a +b+ c
+print(x(5,6,7))
+
+def addition(n):
+    return n + n 
+    
+numbers = [1,2,3,4]
+result = map(addition, numbers)
+result = map(lambda num: num + num) #works the same way
+print(list(result))
+
+
